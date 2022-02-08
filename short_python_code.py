b1b3b0919d4c3858498c7646c97532f014d6fb6d
Nyv <79833594+supernyv@users.noreply.github.com>
2022-02-08 04:09:17 +0100

    Create short_python_code.py

---
+++ short_python_code.py
@@ -0,0 +1,9 @@
+#Creating a file from scratch
+
+print("This is The first file in the repository\n")
+
+def repo():
+  sentence = "Using Git and GitHub, from IBM Data Science Professional Certificate"
+  return sentence
+
+repo()
